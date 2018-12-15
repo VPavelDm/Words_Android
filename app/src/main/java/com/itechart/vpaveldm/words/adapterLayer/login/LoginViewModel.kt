2603b4469e825c0ed7e180b60cf4d133fa597817
Pavel Vaitsikhousky <vpaveldm@gmail.com>
2018-12-15 21:45:46 +0300

    Added progress bar to login view

--- app/src/main/java/com/itechart/vpaveldm/words/adapterLayer/login/LoginViewModel.kt
+++ app/src/main/java/com/itechart/vpaveldm/words/adapterLayer/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.itechart.vpaveldm.words.adapterLayer.login
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import android.content.Context
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
@@ -11,16 +12,25 @@ import com.itechart.vpaveldm.words.R
 class LoginViewModel(private val navController: NavController) : ViewModel() {
 
     val error = ObservableField<String>()
+    val progressBarVisible = ObservableBoolean(false)
     private val auth = FirebaseAuth.getInstance()
 
     fun signIn(context: Context, login: String, password: String) {
+        progressBarVisible.set(true)
         if (login.isEmpty() || password.isEmpty()) {
             error.set(context.getString(R.string.error_message_empty_login_or_password_field))
+            progressBarVisible.set(false)
             return
         }
         auth.signInWithEmailAndPassword(login, password)
-                .addOnSuccessListener { navController.popBackStack() }
-                .addOnFailureListener { error.set(it.localizedMessage) }
+                .addOnSuccessListener {
+                    navController.popBackStack()
+                    progressBarVisible.set(false)
+                }
+                .addOnFailureListener {
+                    error.set(it.localizedMessage)
+                    progressBarVisible.set(false)
+                }
     }
 
 }
