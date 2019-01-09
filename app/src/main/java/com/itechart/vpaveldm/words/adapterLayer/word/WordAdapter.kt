380d350b462cb0a55f2a001e36b2fc781062b455
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2019-01-09 13:11:10 +0300

    Added LivePagedList

--- app/src/main/java/com/itechart/vpaveldm/words/adapterLayer/word/WordAdapter.kt
+++ app/src/main/java/com/itechart/vpaveldm/words/adapterLayer/word/WordAdapter.kt
@@ -1,24 +1,18 @@
 package com.itechart.vpaveldm.words.adapterLayer.word
 
-import android.arch.paging.PagedList
 import android.arch.paging.PagedListAdapter
-import android.os.Handler
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.itechart.vpaveldm.words.R
-import com.itechart.vpaveldm.words.adapterLayer.word.paging.WordPositionalDataSource
 import com.itechart.vpaveldm.words.dataLayer.word.Word
-import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.recycler_item_word.view.*
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import android.os.Looper
 
 
-class WordAdapter() : PagedListAdapter<Word, WordAdapter.WordHolder>(DIFF_UTIL) {
+class WordAdapter : PagedListAdapter<Word, WordAdapter.WordHolder>(DIFF_UTIL) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_word, parent, false)
@@ -38,37 +32,11 @@ class WordAdapter() : PagedListAdapter<Word, WordAdapter.WordHolder>(DIFF_UTIL) 
         }
     }
 
-    companion object {
-        fun create(): WordAdapter {
-            val dataSource = WordPositionalDataSource()
-            val config = PagedList.Config.Builder()
-                .setEnablePlaceholders(false)
-                .setPageSize(10)
-                .build()
-            val wordsList = PagedList.Builder<Int, Word>(dataSource, config)
-                .setFetchExecutor(Executors.newSingleThreadExecutor())
-                .setNotifyExecutor(MainThreadExecutor())
-                .build()
-            val adapter = WordAdapter()
-            adapter.submitList(wordsList)
-            return adapter
-        }
-    }
-
-}
-
-private class MainThreadExecutor : Executor {
-
-    private val mHandler = Handler(Looper.getMainLooper())
-
-    override fun execute(command: Runnable?) {
-        mHandler.post(command);
-    }
-
 }
 
 private val DIFF_UTIL = object : DiffUtil.ItemCallback<Word>() {
     override fun areItemsTheSame(oldWord: Word, newWord: Word): Boolean {
+        Log.i("myAppTAG", "oldWord = ${oldWord.key}, newWord = ${newWord.key}")
         return oldWord.key == newWord.key
     }
 
--- app/src/main/java/com/itechart/vpaveldm/words/uiLayer/word/WordFragment.kt
+++ app/src/main/java/com/itechart/vpaveldm/words/uiLayer/word/WordFragment.kt
@@ -2,23 +2,29 @@ package com.itechart.vpaveldm.words.uiLayer.word
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.arch.paging.LivePagedListBuilder
+import android.arch.paging.PagedList
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.itechart.vpaveldm.words.adapterLayer.word.WordAdapter
 import com.itechart.vpaveldm.words.adapterLayer.word.WordViewModel
+import com.itechart.vpaveldm.words.adapterLayer.word.paging.WordSourceFactory
+import com.itechart.vpaveldm.words.dataLayer.word.Word
 import com.itechart.vpaveldm.words.databinding.FragmentWordBinding
+import java.util.concurrent.Executors
 
 class WordFragment : Fragment() {
 
     private lateinit var listener: IAuthorization
     private lateinit var binding: FragmentWordBinding
     private lateinit var viewModel: WordViewModel
-    private var adapter = WordAdapter.create()
+    private var adapter = WordAdapter()
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -47,12 +53,30 @@ class WordFragment : Fragment() {
             layoutManager = LinearLayoutManager(context)
         }
         binding.handler = viewModel
+        initPageList()
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
         listener.authorized()
+    }
+
+    private fun initPageList() {
+        val sourceFactory = WordSourceFactory()
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(10)
+            .build()
+        val pagedListData = LivePagedListBuilder<Int, Word>(sourceFactory, config)
+            .setFetchExecutor(Executors.newSingleThreadExecutor())
+            .build()
+        pagedListData.observe(this, Observer { pagedList ->
+            pagedList?.let {
+                Log.i("myAppTAG", "pagedList size = ${pagedList.size}")
+                adapter.submitList(it)
+            }
+        })
     }
 
 }
