9b133efefc4d3eae01cd3d2828970c40f2446eee
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2019-01-22 11:18:56 +0300

    Fixed item divider bug

--- app/src/main/java/com/itechart/vpaveldm/words/uiLayer/studyWord/StudyItemDivider.kt
+++ app/src/main/java/com/itechart/vpaveldm/words/uiLayer/studyWord/StudyItemDivider.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
+import com.itechart.vpaveldm.words.R
 
 class StudyItemDivider internal constructor(context: Context) : RecyclerView.ItemDecoration() {
     private val divider: Drawable
@@ -17,14 +18,17 @@ class StudyItemDivider internal constructor(context: Context) : RecyclerView.Ite
 
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDrawOver(c, parent, state)
-        val left = parent.paddingLeft + 30
-        val right = parent.width - parent.paddingRight - 30
         val item = parent.getChildAt(0)
+        if (item.id == R.id.wordLayout) {
 
-        val top = item.bottom + (item.layoutParams as RecyclerView.LayoutParams).bottomMargin
-        val bottom = top + divider.intrinsicHeight
+            val left = parent.paddingLeft + 30
+            val right = parent.width - parent.paddingRight - 30
 
-        divider.setBounds(left, top, right, bottom)
-        divider.draw(c)
+            val top = item.bottom + (item.layoutParams as RecyclerView.LayoutParams).bottomMargin
+            val bottom = top + divider.intrinsicHeight
+
+            divider.setBounds(left, top, right, bottom)
+            divider.draw(c)
+        }
     }
 }
